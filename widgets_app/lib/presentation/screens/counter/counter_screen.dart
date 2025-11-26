024d10b2fb08357860789965c69abb3d2d0e6f3b
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-11-26 16:32:30 +0100

    162-Agregar funcionalidad de contador utilizando Riverpod y ajustar la estructura de la aplicación

--- widgets_app/lib/presentation/screens/counter/counter_screen.dart
+++ widgets_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counte_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter Screen')
-      ),
+      appBar: AppBar(title: const Text('Counter Screen')),
       body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge,)
+        child: Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
