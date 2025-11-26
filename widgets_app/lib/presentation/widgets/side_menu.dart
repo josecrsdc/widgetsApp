492b3d115099b28d1dbc64df08c8752b728fb057
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-11-26 13:55:19 +0100

    158-Agregar menú lateral con opciones y separación en la pantalla de navegación

--- widgets_app/lib/presentation/widgets/side_menu.dart
+++ widgets_app/lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -11,6 +14,8 @@ class _SideMenuState extends State<SideMenu> {
   int navDrawerIndex = 1;
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
       selectedIndex: navDrawerIndex,
       onDestinationSelected: (value) {
@@ -19,13 +24,35 @@ class _SideMenuState extends State<SideMenu> {
         });
       },
       children: [
-        NavigationDrawerDestination(
-          icon: const Icon(Icons.add),
-          label: const Text('Home Screen'),
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
+          child: Text('Main'),
         ),
-        NavigationDrawerDestination(
-          icon: const Icon(Icons.add_shopping_cart_rounded),
-          label: const Text('Otra pantalla'),
+        ...appMenuItems
+          .sublist(0,3)
+          .map((item) => 
+            NavigationDrawerDestination(
+              icon:  Icon(item.icon),
+              label:  Text(item.title),
+            )
+        ),
+
+        Padding(
+          padding: const EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
+        ),
+
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+          child: Text('More options'),
+        ),
+        ...appMenuItems
+          .sublist(3)
+          .map((item) => 
+            NavigationDrawerDestination(
+              icon:  Icon(item.icon),
+              label:  Text(item.title),
+            )
         ),
       ],
     );
