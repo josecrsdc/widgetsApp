79fea62b3e6ca8f35dae1152ce8e398226006ab7
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-11-27 17:02:51 +0100

    FIN SECTION 11 - Refactor: mejorar la gestión del cambio de color y modo oscuro en el ThemeNotifier

--- widgets_app/lib/presentation/screens/theme_changer/theme_changer_screen.dart
+++ widgets_app/lib/presentation/screens/theme_changer/theme_changer_screen.dart
@@ -18,8 +18,7 @@ class ThemeChangerScreen extends ConsumerWidget {
               isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
             ),
             onPressed: () {
-              ref.read(themeNotifierProvider.notifier)
-                .toggleDarkMode();
+              ref.read(themeNotifierProvider.notifier).toggleDarkMode();
             },
           ),
         ],
@@ -35,8 +34,7 @@ class _ThemeChangerView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     final List<Color> colors = ref.watch(colorListProvider);
-    final int selectedColor = ref.watch(selectedColorProvider);
-    final bool isDarkMode = ref.watch(isDarkModeProvider);
+    final selectedColor = ref.watch(themeNotifierProvider).selectedColor;
 
     return ListView.builder(
       itemCount: colors.length,
@@ -50,7 +48,7 @@ class _ThemeChangerView extends ConsumerWidget {
           value: index,
           groupValue: selectedColor,
           onChanged: (value) {
-            ref.read(selectedColorProvider.notifier).state = index;
+            ref.read(themeNotifierProvider.notifier).changeColorIndex(index);
           },
         );
       },
