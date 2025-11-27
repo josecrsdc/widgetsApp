ea75c63203a83df0a3f3c82aa8c25a07e4a9b3f2
Jose carlos Rodriguez <jcrodriguez@opentrends.net>
2025-11-27 16:42:14 +0100

    166-Refactor: eliminar variables no utilizadas y mejorar la gestión del tema en la aplicación

--- widgets_app/lib/presentation/providers/theme_provider.dart
+++ widgets_app/lib/presentation/providers/theme_provider.dart
@@ -3,8 +3,28 @@
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
-final isDarkModeProvider = StateProvider<bool>((ref) => false);
-
+// Listado de colores inmutable
 final colorListProvider = StateProvider((ref) => colorList);
 
+// Un simple boolean
+final isDarkModeProvider = StateProvider<bool>((ref) => false);
+
+// Un simple init
 final selectedColorProvider = StateProvider((ref) => 0);
+
+
+
+
+
+//Un objeto de tipo AppTheme (custom)
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(),
+);
+
+// Controller o notifier
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  // State = new AppTheme
+  ThemeNotifier(): super(AppTheme());
+
+
+}
